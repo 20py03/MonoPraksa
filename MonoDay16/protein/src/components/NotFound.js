002d8857836136d14af334a995d985c0c58d4710
20py03 <jovana.papric2@gmail.com>
2024-02-29 14:57:26 +0100

    Day 19

--- MonoDay16/protein/src/components/NotFound.js
+++ MonoDay16/protein/src/components/NotFound.js
@@ -9,10 +9,12 @@ function NotFound() {
                 <h1><b>404 - Not Found</b></h1>
                 <img className="not-found-image" src={notFound} alt="Not Found" />
             </div>
+            {/*
             <div class="bottom-left">404 - Not Found</div>
             <div class="top-left">404 - Not Found</div>
             <div class="top-right">404 - Not Found</div>
             <div class="bottom-right">404 - Not Found</div>
+            */}
         </div>
     );
 }
